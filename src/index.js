790c72ab8d0f904d06954ff0170f4a86ecb7914b
JuanPa Salazar <juanzjck1996@gmail.com>
2021-03-10 21:53:41 -0500

    feature(like):button like with mutation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './App.js'
-ReactDOM.render(<App/>, document.getElementById('app'))
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+const client = new ApolloClient({
+    uri:'https://insta-mok.vercel.app/graphql'
+})
+ReactDOM.render(
+    <ApolloProvider client={client}>
+     <App/>
+    </ApolloProvider>
+, document.getElementById('app'))
